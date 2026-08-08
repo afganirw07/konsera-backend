c38032cb4a5e277705eaa2d2f93c4d728c9181e0
afganirw07 <afganirwansyah070708@gmail.com>
2026-08-08 11:30:11 +0700

    Implement UserService structure and CreateUser method stub

--- internal/services/user/user_services.go
+++ internal/services/user/user_services.go
@@ -2,4 +2,20 @@ package user
 
 import (
 	"context"
+	utils "konsera-backend/internal/utils"
+	dto "konsera-backend/internal/DTO/user"
+	userRepo "konsera-backend/internal/repository/user"
 )
+
+type UserService struct {
+	repo *userRepo.UserRepository
+}
+
+func NewUserService(repo *userRepo.UserRepository) *UserService {
+	return &UserService{repo: repo}
+}
+
+func (s *UserService) CreateUser(ctx context.Context, user *dto.UserRequest) (*dto.UserResponse, error) {
+	
+
+}
